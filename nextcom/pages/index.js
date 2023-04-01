a4212df3472147229b510350629a27e692b767b4
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-01 23:10:20 +0400

    Add Components to commerce app

--- nextcom/pages/index.js
+++ nextcom/pages/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { Product, HeroBanner, FooterBanner } from '../components'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <>
+        <HeroBanner />
+
+        <div className='products-heading'>
+          <h2>Best selling Products</h2>
+          <p> Speakers of many variants </p>
+        </div>
+
+        <div className='products-container'>
+          {['Product1', 'Product2', 'Product3'].map((product) => product)}
+        </div>
+
+        Footer
+
+        <FooterBanner />
+      </>
+    </div>
   )
 }
 
