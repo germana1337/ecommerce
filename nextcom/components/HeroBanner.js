45d96c27c3891a698be8eb6798eb6111fea8b2e9
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-01 23:18:34 +0400

    imported nextlink to  herobANNER

--- nextcom/components/HeroBanner.js
+++ nextcom/components/HeroBanner.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Link from 'next/link'
+
+
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src='' alt='headphones' className='hero-banner-image' />
+
+        <div>
+          <Link href='/product /ID'> 
+           <button type='button'>Button TEXT</button>
+           </Link>
+           <div>
+            
+           </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
