ca03043d031aad2a7257c47139e4c227ad8dc19c
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-02 00:39:41 +0400

    succesfully add navigations from sanity studio and ready to build a Product component

---
+++ nextcom/lib/client.js
@@ -0,0 +1,16 @@
+import sanityClient from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
+
+
+export const client = sanityClient({
+    projectId: '6q3gz3di',
+    dataset: 'production',
+    apiVersion: '2023-04-01',
+    useCdn: true,
+    toke: process.env.NEXT_PUBLIC_SANITY_TOKEN
+
+})
+
+const builder = imageUrlBuilder(client)
+
+export const urlFor = (source) => builder.image(source);
--- nextcom/pages/index.js
+++ nextcom/pages/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { client } from '../lib/client'
 
 import { Product, HeroBanner, FooterBanner } from '../components'
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <div>
       <>
-        <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
+        {console.log(bannerData)}
+
 
         <div className='products-heading'>
           <h2>Best selling Products</h2>
@@ -13,7 +17,7 @@ const Home = () => {
         </div>
 
         <div className='products-container'>
-          {['Product1', 'Product2', 'Product3'].map((product) => product)}
+          {products?.map((product) => product.className)}
         </div>
 
         Footer
@@ -22,6 +26,18 @@ const Home = () => {
       </>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
