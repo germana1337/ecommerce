1366edde9d3a3e1b3c33ef97ac0b79e769cf7e87
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-02 22:04:09 +0400

    Items will add cart logic started

--- nextcom/context/StateContext.js
+++ nextcom/context/StateContext.js
@@ -1,41 +1,49 @@
-
 import React, { createContext, useContext, useState, useEffect } from 'react'
 import { toast } from 'react-hot-toast'
 const Context = createContext();
 
-
 export const StateContext = ({ children }) => {
 
-    //! # This show cart state is used to show and hide the cart
     const [showCart, setShowCart] = useState(false);
-
-    //! # ეს cartItems მდგომარეობა გამოიყენება კალათაში ნივთების შესანახად
-    const [cartItems, setCartItems] = useState();
-
-    //? # ეს TotalPrice მდგომარეობა გამოიყენება კალათაში საქონლის მთლიანი ფასის შესანახად
-    const [totalPrice, setTotalPrice] = useState();
-
-    //? # ეს totalQuantities მდგომარეობა გამოიყენება კალათაში ნივთების მთლიანი რაოდენობის შესანახად
-    const [totalQuantities, setTotalQuantities] = useState();
-
-    //? # ეს რაოდენობრივი მდგომარეობა გამოიყენება კალათაში ნივთების რაოდენობის შესანახად
+    const [cartItems, setCartItems] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
-    //! # This function is used to show the cart
-    const incQty = () => {
-        setQty((prevQty) => prevQty + 1); //? # ეს გამოიყენება კალათაში არსებული ნივთების რაოდენობის გასაზრდელად
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+        setTotalPrice((prevPrice) => prevPrice + product.price * quantity);
+        setTotalQuantities((prevQuantities) => prevQuantities + quantity);
+        if (checkProductInCart) {
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) {
+                    return {
+                        ...cartProduct,
+                        quantity: cartProduct.quantity + quantity,
+                    };
+                }
+                return cartProduct;
+            });
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+            setCartItems([...cartItems, { ...product }]);
+        }
+        toast.success(`${quantity} ${product.name} added to cart`);
     }
 
-     // # This function with this check logic is used to decrease the quantity of the items in the cart and if the quantity is less than 1 then it will not decrease the quantity
+    const incQty = () => {
+        setQty((prevQty) => prevQty + 1);
+    }
+
     const decQty = () => {
         setQty((prevQty) => {
-          if(prevQty - 1 < 1 ) return 1;
-            return prevQty - 1; 
-        }); 
-    } 
+            if (prevQty - 1 < 1) return 1;
+            return prevQty - 1;
+        });
+    }
 
     return (
-        //! # Context Provider is used to provide the state to the children components and also to update the state
         <Context.Provider
             value={{
                 showCart,
@@ -45,13 +53,12 @@ export const StateContext = ({ children }) => {
                 qty,
                 incQty,
                 decQty,
+                onAdd,
             }}
         >
             {children}
         </Context.Provider>
-
     )
 }
 
-export const useStateContext = () => useContext(Context); //? # This is used to use the state in the children components and also to update the state in the children components
-
+export const useStateContext = () => useContext(Context);
--- nextcom/pages/product/[slug].js
+++ nextcom/pages/product/[slug].js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { useState } from 'react'
-import { client, urlFor } from '../../lib/client'
-import { aiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar, AiOutlineMinus } from 'react-icons/ai'
-import { Product } from '../../components'
-import { useStateContext } from '../../context/StateContext'
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+
+import { client, urlFor } from '../../lib/client';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
-
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty } = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true);
+    }
 
     return (
         <div>
-            <div className='product-detail-container'>
+            <div className="product-detail-container">
                 <div>
-                    <div className='image-container'>
-                        <img src={urlFor(image && image[index])} className='product-detail-image' />
+                    <div className="image-container">
+                        <img src={urlFor(image && image[index])} className="product-detail-image" />
                     </div>
-                    <div className='small-images-container'>
-                        {image && image.map((item, i) => (
-                            <img src={urlFor(item)}
+                    <div className="small-images-container">
+                        {image?.map((item, i) => (
+                            <img
+                                key={i}
+                                src={urlFor(item)}
                                 className={i === index ? 'small-image selected-image' : 'small-image'}
-                                onMouseOver={() => setIndex(i)}
+                                onMouseEnter={() => setIndex(i)}
                             />
                         ))}
                     </div>
                 </div>
 
-                <div className='product-detail-desc'>
+                <div className="product-detail-desc">
                     <h1>{name}</h1>
-                    <div className='reviews'>
+                    <div className="reviews">
                         <div>
                             <AiFillStar />
                             <AiFillStar />
@@ -42,34 +49,30 @@ const ProductDetails = ({ product, products }) => {
                             (20)
                         </p>
                     </div>
-                    <h4>Details:  </h4>
+                    <h4>Details: </h4>
                     <p>{details}</p>
-                    <p className='price'>${price}</p>
-                    <div className='quantity'>
-                        <h3>
-                            Quantity:
-                        </h3>
-                        <p className='quantity-desc'>
-                            <span className='minus' onClick={decQty}> <AiOutlineMinus /> </span>
-                            <span className='num' onClick={incQty}>{qty}</span>
-                            <span className='plus' onClick={incQty}> <AiOutlinePlus /> </span>
+                    <p className="price">${price}</p>
+                    <div className="quantity">
+                        <h3>Quantity:</h3>
+                        <p className="quantity-desc">
+                            <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
+                            <span className="num">{qty}</span>
+                            <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
                         </p>
-
                     </div>
-                    <div className='buttons'>
-                        <button type='button' className='add-to-cart' onClick=''> Add to Cart </button>
-                        <button type='button' className='buy-now' onClick=''> Buy Now </button>
+                    <div className="buttons">
+                        <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
+                        <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
                     </div>
                 </div>
             </div>
-            <div className='maylike-products-wrapper'>
-                <h2> You may also like </h2>
-                <div className='marquee'>
-                    <div className='maylike-products-container  track'>
+
+            <div className="maylike-products-wrapper">
+                <h2>You may also like</h2>
+                <div className="marquee">
+                    <div className="maylike-products-container track">
                         {products.map((item) => (
-                            <Product key={item._id}
-                                product={item}
-                            />
+                            <Product key={item._id} product={item} />
                         ))}
                     </div>
                 </div>
@@ -77,6 +80,7 @@ const ProductDetails = ({ product, products }) => {
         </div>
     )
 }
+
 
 export const getStaticPaths = async () => {
     const query = `*[_type == "product"]
