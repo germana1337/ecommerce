82e4f02d30809f6a11d079c3b72a518dd4d0e3b2
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-07 01:28:25 +0400

    Payment method added

--- nextcom/components/Cart.js
+++ nextcom/components/Cart.js
@@ -1,16 +1,80 @@
-import React, { useRef } from 'react'
-import Link from 'next/link'
-import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
-import { TiDeleteOutline } from 'react-icons/ti'
-import { toast } from 'react-hot-toast'
-import { useStateContext } from '../context/StateContext'
-import { urlFor } from '@/lib/client'
-
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import { toast } from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '@/lib/client';
+import getStripe from '../lib/getStripe';
+import stripe from '../pages/api/stripe'
+import axios from 'axios';
 
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
+  const { totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuanitity,
+    onRemove }
+    = useStateContext();
+
+
+
+  // const handleCheckout = async () => {
+  //   const stripe = await getStripe();
+  //   console.log(cartItems);
+  //   try {
+  //     const { data } = await axios.post(
+  //       "/api/stripe",
+  //       JSON.stringify({ cartItems })
+  //     );
+
+  //     if (data.status === 500) {
+  //       return toast.error('Something went wrong');
+  //     }
+
+  //     toast.loading('Redirecting to checkout...');
+
+  //     stripe.redirectToCheckout({ sessionId: data.id });
+  //   } catch (error) {
+  //     console.error('Error during checkout:', error);
+  //     toast.error('Something went wrong');
+  //   }
+  // };
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+    
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+
+  // const createPaymentIntent = async () => {
+  //   try {
+  //     const { data } = await axios.post(
+  //       "/api/stripe",
+  //       JSON.stringify({ cartItems })
+  //     );
+
+  //   } catch (error) {
+  //     console.log(error.response);
+  //   }
+  // };
+
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -39,6 +103,7 @@ const Cart = () => {
             </Link>
           </div>
         )}
+
         <div className="product-container">
           {cartItems.length >= 1 && cartItems.map((item) => (
             <div className="product" key={item._id}>
@@ -54,7 +119,7 @@ const Cart = () => {
                       <span className="minus" onClick={() => toggleCartItemQuanitity(item._id, 'dec')}>
                         <AiOutlineMinus />
                       </span>
-                      <span className="num" onClick="">{item.quantity}</span>
+                      <span className="num" >{item.quantity}</span>
                       <span className="plus" onClick={() => toggleCartItemQuanitity(item._id, 'inc')}><AiOutlinePlus /></span>
                     </p>
                   </div>
@@ -68,20 +133,21 @@ const Cart = () => {
                 </div>
               </div>
             </div>
-
           ))}
         </div>
+
         {cartItems.length >= 1 && (
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
               <h3>${totalPrice}</h3>
             </div>
-            <div className='btn-container'>
-              <button type='button' className='btn' onClick=''>
-                Pay with Stripe
-              </button>
-
+            <div className='btn-container' >
+              <form action="/api/stripe" method="POST">
+                <button type='button' className='btn' onClick={handleCheckout} >
+                  Pay with Stripe
+                </button>
+              </form>
             </div>
           </div>
         )}
--- nextcom/context/StateContext.js
+++ nextcom/context/StateContext.js
@@ -1,110 +1,84 @@
+import React, { createContext, useContext, useState } from 'react';
+import { toast } from 'react-hot-toast';
 import product from '@/sanity-ecom/schemas/product';
-import React, { createContext, useContext, useState, useEffect } from 'react'
-import { toast } from 'react-hot-toast'
+
 const Context = createContext();
 
+//* # I make this state context component to hold the state of the cart. I use the useState hook to create the state variables. I then create the functions that will be used to update the state variables. I then return the state variables and the functions in an object. I then wrap the children in the context provider and pass the object as the value. I then export the context and the context provider. I then import the context provider into the _app.js file and wrap the children in it. I then import the context into the components that need to access the state variables and functions. I then destructure the state variables and functions from the context. I then use the state variables and functions in the components.
 export const StateContext = ({ children }) => {
-
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantities, setTotalQuantities] = useState(0);
-    const [qty, setQty] = useState(1);
 
-    let foundProduct;
-    let index;
-
+    //? # This onAdd function is for adding a product to the cart. It takes in the product and the quantity as arguments. It then checks if the product is already in the cart. If it is, it then maps through the cart items and updates the quantity of the product in the cart. If it is not, it then adds the product to the cart. It then updates the total price and total quantities in the cart. It then uses the toast library to display a success message.
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
-        setTotalPrice((prevPrice) => prevPrice + product.price * quantity);
-        setTotalQuantities((prevQuantities) => prevQuantities + quantity);
-        if (checkProductInCart) {
-            const updatedCartItems = cartItems.map((cartProduct) => {
-                if (cartProduct._id === product._id) {
-                    return {
-                        ...cartProduct,
-                        quantity: cartProduct.quantity + quantity,
-                    };
-                }
-                return cartProduct;
-            });
-            setCartItems(updatedCartItems);
-        } else {
-            product.quantity = quantity;
-            setCartItems([...cartItems, { ...product }]);
-        }
+        const updatedCartItems = checkProductInCart
+            ? cartItems.map((cartProduct) =>
+                cartProduct._id === product._id
+                    ? { ...cartProduct, quantity: cartProduct.quantity + quantity }
+                    : cartProduct
+            )
+            : [...cartItems, { ...product, quantity }];
+        //* # this newtotalprice and newtotalqountities are variables that are used to update the total price and total quantities in the cart. They are set to the current total price and total quantities in the cart plus the price of the product times the quantity of the product. They are then used to update the total price and total quantities in the cart.
+        const newTotalPrice = totalPrice + product.price * quantity.toFixed(2)
+        const newTotalQuantities = totalQuantities + quantity;
+        setCartItems(updatedCartItems);
+        setTotalPrice(newTotalPrice);
+        setTotalQuantities(newTotalQuantities);
         toast.success(`${quantity} ${product.name} added to cart`);
-    }
-
+    };
+    //* # With using this onRemove function, I can remove a product from the cart. It takes in the product as an argument. It then finds the product in the cartItems array that matches the id of the product that was passed in. It then creates a new array of cartItems and filters out the product that was passed in. It then sets the cartItems to the new array, the total price to the new total price, and the total quantities to the new total quantities.
     const onRemove = (product) => {
-        foundProduct = cartItems.find((item) => item._id === product._id);
+        const foundProduct = cartItems.find((item) => item._id === product._id);
         const newCartItems = cartItems.filter((item) => item._id !== product._id);
-
-        setTotalPrice((prevPrice) => prevPrice - foundProduct.price * foundProduct.quantity);
-        setTotalQuantities((prevQuantities) => prevQuantities - foundProduct.quantity);
+        const newTotalPrice = totalPrice - foundProduct.price * foundProduct.quantity;
+        const newTotalQuantities = totalQuantities - foundProduct.quantity;
         setCartItems(newCartItems);
-    }
-
-    // # this togglecartitemquantity function is  for the cart page to increase and decrease the quantity of the product in the cart.
+        setTotalPrice(newTotalPrice);
+        setTotalQuantities(newTotalQuantities);
+    };
+    // # Here togglecartitemquanitity is a function that takes in an id and a value. It then finds the product in the cartItems array that matches the id. If it doesn't find a match, it returns. If it does find a match, it then finds the index of the product in the cartItems array. It then creates a new array of cartItems and sets the quantity of the product to the new quantity. It then sets the cartItems to the new array, the total price to the new total price, and the total quantities to the new total quantities.
     const toggleCartItemQuanitity = (id, value) => {
-        foundProduct = cartItems.find((item) => item._id === id);
-        index = cartItems.findIndex((item) => item._id === id);
-        const newCartItemsSplice = cartItems.filter((item) => item._id !== id);
-
-        // # This if statement logic is for the increase button in the cart page and it will increase the quantity of the product in the cart.
-        if (value === 'inc') {
-            let newCartItems = [...newCartItemsSplice,
-            { ...foundProduct, quantity: foundProduct.quantity + 1 }];
+        const foundProduct = cartItems.find((item) => item._id === id);
+        if (!foundProduct) return;
+        const index = cartItems.indexOf(foundProduct);
+        const newCartItems = [...cartItems];
+        const newTotalPrice = totalPrice + (value === 'inc' ? foundProduct.price : -foundProduct.price);
+        const newTotalQuantities = totalQuantities + (value === 'inc' ? 1 : -1);
+        const newQuantity = foundProduct.quantity + (value === 'inc' ? 1 : -1);
+        if (newQuantity > 0) {
+            newCartItems.splice(index, 1, { ...foundProduct, quantity: newQuantity });
             setCartItems(newCartItems);
-            // # This setTotalPrice and setTotalQuantities are for the total price and total quantity of the product in the cart. 
-            setTotalPrice((prevPrice) => prevPrice + foundProduct.price);
-            setTotalQuantities((prevQuantities) => prevQuantities + 1);
-
-            // # this else if statement logic is for the decrease button in the cart page and it will decrease the quantity of the product in the cart.
-        } else if (value === 'dec') {
-            // # This if statement inside of else if statement is for to check the quantity of the product in the cart is 1 or not. and if the quantity is 1 then it will not decrease the quantity. 
-            if (foundProduct.quantity > 1) {
-                const newCartItems = cartItems
-                    .filter((item) => item._id !== id)
-                    .concat({ ...foundProduct, quantity: foundProduct.quantity - 1 });
-                setCartItems(newCartItems);
-
-                setTotalPrice((prevPrice) => prevPrice - foundProduct.price);
-                setTotalQuantities((prevQuantities) => prevQuantities - 1);
-            }
+            setTotalPrice(newTotalPrice);
+            setTotalQuantities(newTotalQuantities);
         }
-    }
+    };
 
-    const incQty = () => {
-        setQty((prevQty) => prevQty + 1);
-    }
+    const incQty = () => setQty((prevQty) => prevQty + 1);
 
-    const decQty = () => {
-        setQty((prevQty) => {
-            if (prevQty - 1 < 1) return 1;
-            return prevQty - 1;
-        });
-    }
+    const decQty = () =>
+        setQty((prevQty) => (prevQty > 1 ? prevQty - 1 : 1));
 
-    return (
-        <Context.Provider
-            value={{
-                showCart,
-                setShowCart,
-                cartItems,
-                totalPrice,
-                totalQuantities,
-                qty,
-                incQty,
-                decQty,
-                onAdd,
-                toggleCartItemQuanitity,
-                onRemove,
-            }}
-        >
-            {children}
-        </Context.Provider>
-    )
-}
+    const [qty, setQty] = useState(1);
+
+    // # This value is what is passed to the provider. It is an object that contains all the state variables and functions that are used in the app.
+    const value = {
+        showCart,
+        setShowCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+        toggleCartItemQuanitity,
+        onRemove,
+    };
+    //! # This is the provider that wraps the children and passes the value to the children and i use it in the _app.js file.
+    return <Context.Provider value={value}>{children}</Context.Provider>;
+};
 
 export const useStateContext = () => useContext(Context);
