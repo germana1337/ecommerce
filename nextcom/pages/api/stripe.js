f42f47d832d304affac72a64b09c936fac2d8be1
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-07 01:37:49 +0400

    shipping rate added

--- nextcom/pages/api/stripe.js
+++ nextcom/pages/api/stripe.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
                     {
                       
                         shipping_rate: 'shr_1Mta4LL2b18r387uAvcNoVDo',
+                        shipping_rate: 'shr_1Mta4LL2b18r387uAvcNoVDo',
                     }
                 ],
                 line_items: req.body.map((item) => {
@@ -55,3 +56,4 @@ export default async function handler(req, res) {
         res.status(405).end('Method Not Allowed');
     }
 }
+
